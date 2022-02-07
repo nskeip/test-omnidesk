85259feaba177db02064742dfd21a8c3486c4e8a
Nikita Hismatov <hismatovns@gmail.com>
2022-02-07 14:05:43 +0500

    make_dummy_cases typing & doc

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import base64
 import itertools
 import random
 import string
-from datetime import datetime, timedelta
+import json
+from datetime import date, datetime, timedelta
+from typing import List
+from urllib.request import urlopen, Request
 
 from settings import *
 
@@ -11,18 +15,18 @@ except ImportError:
     pass
 
 
-def make_dummy_cases(since: datetime):
+def make_dummy_cases(date_from: date) -> List[dict]:
     """
     Генератор тестовых обращений:
 
-    Первому присваивается дата `since`.
+    Первому присваивается дата `date_from`.
 
-    Второму - дата `since` + 1 день.
-    Обращению `n` присваивается дата `since` + `n`, если эта дата
+    Второму - дата `date_from` + 1 день.
+    Обращению `n` присваивается дата `date_from` + `n`, если эта дата
     не превосходит сегодняшнего числа. Иначе присваивание дат
-    для этого номера и далее снова возвращается на дату `since`.
+    для этого номера и далее снова возвращается на дату `date_from`.
 
-    Или: дата обращения i = since + ((i % (now - since + 1)) дней)
+    Или: дата обращения i = date_from + ((i % (today - date_from + 1)) дней)
     """
 
     FIRST_NAMES = [
@@ -40,7 +44,7 @@ def make_dummy_cases(since: datetime):
         'yahoo.com',
     ]
 
-    now = datetime.now()
+    today = date.today()
 
     for i in itertools.count():
         f_name = random.choice(FIRST_NAMES)
@@ -56,7 +60,8 @@ def make_dummy_cases(since: datetime):
         content = f'Lorem ipsum #{i}'
         content_html = f'<span>{content}</span>'
 
-        created_at = since + timedelta(days=i % ((now - since).days + 1))
+        created_at_date = date_from + timedelta(days=i % ((today - date_from).days + 1))
+        created_at = datetime.combine(created_at_date, datetime.min.time())
 
         yield {
             'email': email,
@@ -70,5 +75,25 @@ def make_dummy_cases(since: datetime):
         }
 
 
+def omni_request(path_without_slash):
+    url = f'https://{OMNIDESK_DOMAIN}.omnidesk.ru/api/{path_without_slash}'
+
+    req = Request(url)
+
+    b64_auth_str = base64.b64encode(
+        bytes(f'{OMNIDESK_EMAIL}:{OMNIDESK_API_KEY}', 'utf-8')
+    ).decode('utf-8')
+    req.add_header('Authorization', f'Basic {b64_auth_str}')
+    req.add_header('Accept', 'application/json')
+
+    with urlopen(req) as con:
+        resp = con.read()
+        return json.loads(resp.decode('utf-8'))
+
+
 if __name__ == '__main__':
-    pass
+    # cs = make_dummy_cases(date(2022, 2, 1))
+    # for case_i in range(10):
+    #     print(next(cs)['created_at'])
+    data = omni_request('cases.json')
+    print('')
