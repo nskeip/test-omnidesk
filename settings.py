d7f64108c207a37690300bc0c8bfeb8d1f109f84
Nikita Hismatov <hismatovns@gmail.com>
2022-02-08 13:34:21 +0500

    DATABASE_PATH via pathlib

--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
+import pathlib
+
+_project_abs_path = pathlib.Path(__file__).parent.resolve()
+
 OMNIDESK_DOMAIN = 'example'
 OMNIDESK_EMAIL = 'example@example.com'
 OMNIDESK_API_KEY = 'EXAMPLE_API_KEY'
-DATABASE_PATH = './db.sqlite'
+DATABASE_PATH = str(_project_abs_path / 'db.sqlite')
